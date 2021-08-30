52f2c67d4d4d07703a07a1b56b669ed9bac4737e
Carlos <contact@carloscolin.me>
2021-08-30 00:28:11 -0500

    Detached router from the server

--- server.js
+++ server.js
@@ -1,25 +1,11 @@
 const express = require('express');
-const router = express.Router();
-
-const response = require('./network/response');
+const router = require('./network/routes');
 
 let app = express();
 
-app.use(router);
 app.use(express.json());
 
-router.get('/', (req, res, next) => {
-    if (req.query.error == "ok") {
-        response.error(req, res, "Everything failed!", 400, "More details!");
-    } else {
-        response.success(req, res, "Excelente", 201);
-    }
-})
-
-router.post('/', (req, res, next) => {
-    console.log(req.body);
-    res.send('Hola desde post')
-})
+router(app);
 
 app.use('/app', express.static('public'));
 
