9ced8c305e1757309bc3afc4b09b22bfec3cbb6b
Carlos <contact@carloscolin.me>
2021-08-30 00:56:16 -0500

    Implemented get & post methods for messages using controller

--- components/message/network.js
+++ components/message/network.js
@@ -10,11 +10,15 @@ const controller = require('./controller');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    if (req.query.error == "ok") {
-        response.error(req, res, "Everything failed!", 400, "More details!");
-    } else {
-        response.success(req, res, "Very good!", 201);
-    }
+
+    controller.getMessages()
+        .then((data) => {
+            response.success(req, res, data, 200);
+        })
+        .catch((err) => {
+            response.error(req, res, err.message, err.status_code, err.details);
+        })
+        
 })
 
 router.post('/', (req, res, next) => {
