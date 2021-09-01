41c0212c88efd732102dec3adf662e22047147b1
Carlos <contact@carloscolin.me>
2021-09-01 17:08:25 -0500

    Use config file

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const cors = require('cors');
 // Require and config dotenv
 require('dotenv').config()
 
-const router = require('./network/routes');
-const socket = require('./socket');
 const db = require('./db');
+const socket = require('./socket');
+const config = require('./config');
+const router = require('./network/routes');
 
 /**
  * Connect to db
  */
-const url = `${process.env.MONGO_DB_HOST}://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_REMAIN}`;
-db.connect(url);
+db.connect(config.db_url);
 
 /**
  * Enable CORS
@@ -35,12 +35,12 @@ router(app);
  */
 socket.connect(server);
 
-app.use('/app', express.static('public'));
+app.use(config.public_route, express.static('public'));
 
 /**
  * App config
  */
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`Server is listening at http://localhost:${PORT}`);
+    console.log(`Server is listening at ${config.host}:${config.port}`);
 });
